8f1225c5362863b7d86358c21d9cdb7d3699e992
25349023 <25349023.qq@gmail.com>
2025-10-19 13:01:31 +0800

    [UPDATE] git clone first

--- lift-the-fog.sh
+++ lift-the-fog.sh
@@ -8,6 +8,17 @@ set -eEuo pipefail
 
 sudo -v
 
+sudo apt install -y git 
+
+cd /tmp
+git clone https://github.com/25349023/ltf.git
+cd ltf
+cp configs/.gitconfig ~
+
+sudo apt install -y vim tmux
+cp configs/.vimrc ~
+cp configs/.tmux.conf ~
+
 cp ~/.bashrc ~/.bashrc.old
 cat configs/.bashrc.patch >> ~/.bashrc 
 
@@ -15,14 +26,8 @@ sudo apt update
 sudo apt install -y etckeeper
 sudo etckeeper init
 
-sudo apt install -y git vim tmux
-
-cp configs/.vimrc ~
-cp configs/.gitconfig ~
-cp configs/.tmux.conf ~
-
-
 sudo apt install -y firewalld
 sudo firewall-cmd --add-service=mdns --permanent
 sudo firewall-cmd --add-port=5900/udp --permanent  # for vnc server
 sudo firewall-cmd --add-port=5900/tcp --permanent  # for vnc server
+
